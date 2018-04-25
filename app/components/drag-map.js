f86c11178bffe02ee0f5642f107e292749f87996
Isaac Ezer <isaac.ezer@gmail.com>
2018-04-24 23:01:19 -0400

    detect overlap

--- app/components/drag-map.js
+++ app/components/drag-map.js
@@ -1,6 +1,39 @@
 import Component from '@ember/component';
 // http://www.petercollingridge.co.uk/interactive-svg-components/draggable-svg-element
+
 const REGEX = /\{(.+)\}/;
+
+const LABEL_DATA_ATTRIBUTE = 'data-dragnet-label';
+
+const DRAGGABLE_CLASS = 'dragnet__label';
+
+function getX(index) {
+  return 0;
+}
+
+function getY(index) {
+  return 25 * (index + 1);
+}
+
+let detectOverlap = (function () {
+    function getPositions(elem) {
+        let pos = elem.getBoundingClientRect();
+        return [[pos.left, pos.right], [pos.top, pos.bottom]];
+    }
+
+    function comparePositions(p1, p2) {
+        let r1, r2;
+        r1 = p1[0] < p2[0] ? p1 : p2;
+        r2 = p1[0] < p2[0] ? p2 : p1;
+        return r1[1] > r2[0] || r1[0] === r2[0];
+    }
+
+    return function (a, b) {
+        let pos1 = getPositions(a),
+            pos2 = getPositions(b);
+        return comparePositions(pos1[0], pos2[0]) && comparePositions(pos1[1], pos2[1]);
+    };
+})();
 
 function parseLabels(inputSVG, targetSVG) {
   inputSVG.querySelectorAll('text').forEach((text, i) => {
@@ -8,9 +41,9 @@ function parseLabels(inputSVG, targetSVG) {
     if (!match) { return; }
     let answer = match[1];
     text.textContent = "--";
-    text.setAttribute('data-dragnet-label', answer);
+    text.setAttribute(LABEL_DATA_ATTRIBUTE, answer);
     targetSVG.insertAdjacentHTML('beforeend', `<text transform="matrix(1 0 0 1 0 0)"
- class="dragnet__draggable" x="0" y="${25 * (i + 1)}">${answer}</text>`);
+ class="${DRAGGABLE_CLASS}" x="${getX(i)}" y="${getY(i)}">${answer}</text>`);
   });
 }
 
@@ -23,7 +56,7 @@ export default Component.extend({
   },
 
   mouseDown(evt) {
-    if (!evt.target.classList.contains('dragnet__draggable')) {
+    if (!evt.target.classList.contains(DRAGGABLE_CLASS)) {
       return;
     }
 
@@ -39,6 +72,13 @@ export default Component.extend({
 
   mouseMove(evt) {
     if (!this.selectedElement) { return; }
+
+    this.element.querySelectorAll(`[${LABEL_DATA_ATTRIBUTE}]`).forEach(label => {
+      if (detectOverlap(label, this.selectedElement)) {
+        console.log('overlap');
+      }
+    });
+
     let dx = evt.clientX - this.currentX;
     let dy = evt.clientY - this.currentY;
     this.currentMatrix[4] += dx;
@@ -57,7 +97,7 @@ export default Component.extend({
 
   mouseUp(evt) {
     if (!this.selectedElement) { return; }
-    this.resetPosition(this.selectedElement);
+    // this.resetPosition(this.selectedElement);
     this.selectedElement = null;
   }
 
